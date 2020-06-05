580174487f526e81c715b21c6321d23ad816d459
andy <z5161179@education.unsw.edu.au>
2020-06-06 01:36:35 +1000

    q1 done

--- ass1/q1.py
+++ ass1/q1.py
@@ -11,18 +11,19 @@ def main():
 	print(q1(arr))
 
 def q1(arr):
-	seen = {}
+	seen = {} # store square sums found so far
 
+	# n^2 loop through array
 	for i, num1 in enumerate(arr):
 		for j, num2 in enumerate(arr[i + 1:]):
 			total = num1*num1 + num2*num2
-			if total in seen:
-				return True
 			
+			if total in seen: # if we have previously made the sum, we have satisfied condition
+				return True	
+
 			seen[total] = (num1, num2)			
 	
 	return False
-
 
 if __name__ == '__main__':
 	main()
